a896bc1f79a45e3c75dcc23d8f466613266ed040
James Caleca <jacaleca2@gmail.com>
2023-04-20 12:20:33 -0600

    Switch App.js and App2.js

---
+++ client/src/App2.js
@@ -0,0 +1,67 @@
+import React, { useContext } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Auth from './pages/Auth'
+import Profile from './pages/Profile'
+import Public from './pages/Public'
+import Post from './pages/Post'
+import IssueList from './components/IssueList'
+import IssueDetailPage from './pages/IssueDetailPage'
+import NotFound from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import Layout from './components/Layout'
+import { UserContext } from './context/UserProvider'
+import './css/styles.css'
+
+function App() {
+  const { token, user } = useContext(UserContext)
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route 
+              path='/'
+              element={token ? <Navigate to='/issues' /> : <Auth />}
+            />
+            
+            <Route 
+              path='/issues' 
+              element={<ProtectedRoute token={token} redirectTo='/'>
+                  <Public />
+                </ProtectedRoute>
+              } 
+            />
+            <Route 
+              path='/issues/:issueId'
+              element={<ProtectedRoute token={token} redirectTo='/'>
+                  <IssueDetailPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route 
+              path='/profile'
+              element={<ProtectedRoute token={token} redirectTo='/'>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route 
+              path='/post'
+              element={<ProtectedRoute token={token} redirectTo='/'>
+                  <Post />
+                </ProtectedRoute>
+              }
+            />
+            <Route 
+              path='*'
+              element={<NotFound />}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
